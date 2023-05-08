a3f15da5941ea65cbc3a84b67ac9111b33199724
wellers <paul_welbourne@yahoo.co.uk>
2023-05-08 20:57:50 +0100

    Initial commit.

---
+++ Client/Program.cs
@@ -0,0 +1,14 @@
+﻿using Grpc.Core;
+using MyPackage;
+
+Channel channel = new Channel("localhost", 50051, ChannelCredentials.Insecure);
+var client = new MyService.MyServiceClient(channel);
+
+HelloRequest request = new HelloRequest { Name = "Alice" };
+HelloResponse response = client.Hello(request);
+
+Console.WriteLine(response.Message);
+
+channel.ShutdownAsync().Wait();
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
---
+++ Service/Program.cs
@@ -0,0 +1,29 @@
+﻿using Grpc.Core;
+using MyPackage;
+
+Server server = new()
+{
+    Services = { MyService.BindService(new MyServiceImplementation()) },
+    Ports = { new ServerPort("localhost", 50051, ServerCredentials.Insecure) }
+};
+server.Start();
+
+Console.WriteLine("Server listening on port 50051");
+
+Console.WriteLine("Press any key to stop the server...");
+Console.ReadKey();
+
+server.ShutdownAsync().Wait();
+
+public class MyServiceImplementation : MyService.MyServiceBase
+{
+    public override Task<HelloResponse> Hello(HelloRequest request, ServerCallContext context)
+    {
+        Console.WriteLine($"Request received from {request.Name}...");
+
+        return Task.FromResult(new HelloResponse
+        {
+            Message = "Hello, " + request.Name
+        });
+    }
+}
